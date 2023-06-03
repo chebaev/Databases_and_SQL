8b854563e21925567adc705597e1cd0e90039ea0
chebaev <106076557+chebaev@users.noreply.github.com>
2023-06-03 23:04:04 +0500

    Update Homework5.sql

--- Homework5.sql
+++ Homework5.sql
@@ -1,30 +1,33 @@
 -- Скрипт: https://drive.google.com/file/d/1rkNr6GHSE9ulRs39BB4RC80aKMg1c7Jw/view?usp=share_link
 DROP DATABASE IF EXISTS Homework5;
 CREATE DATABASE IF NOT EXISTS Homework5;
+
 USE Homework5;
 
 DROP TABLE IF EXISTS `cars`;
 CREATE TABLE cars
 (
-	id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY,
     name VARCHAR(45),
     cost INT
 );
 
 INSERT cars
 VALUES
-	(1, "Audi", 52642),
+    (1, "Audi", 52642),
     (2, "Mercedes", 57127 ),
     (3, "Skoda", 9000 ),
     (4, "Volvo", 29000),
-	(5, "Bentley", 350000),
+    (5, "Bentley", 350000),
     (6, "Citroen ", 21000 ), 
     (7, "Hummer", 41400), 
     (8, "Volkswagen ", 21600);
     
 SELECT *
 FROM cars;
+
 -- 1. Создайте представление, в которое попадут автомобили стоимостью до 25 000 долларов
+
 CREATE VIEW cars_view AS
 ( 
     SELECT *
@@ -39,17 +42,19 @@ FROM cars_view;
 
 ALTER VIEW cars_view AS
 (
-	SELECT *
+    SELECT *
     FROM cars
     WHERE cost < 30000
 );
 
 SELECT *
 FROM cars_view;
+
 -- 3. Создайте представление, в котором будут только автомобили марки “Шкода” и “Ауди”
+
 CREATE VIEW cars_view_2 AS
 (
-	SELECT *
+    SELECT *
     FROM cars
     WHERE name IN ("Skoda", "Audi")
 );
